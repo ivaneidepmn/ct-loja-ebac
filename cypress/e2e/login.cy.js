43649ea2ada70d4eaa880aa1f7b85eb536c88e8a
Ivaneide Pires Monteiro do Nascimento <84043539+ivaneidepmn@users.noreply.github.com>
2024-02-21 18:51:31 -0300

    Criando arquivos casos de teste

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress"/>
+describe('Funcionalidade: login', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    it('', () => {
+        
+    });
+});
